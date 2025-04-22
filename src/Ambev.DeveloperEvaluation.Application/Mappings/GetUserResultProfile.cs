41bced15a8c47cf7289747b6cff5f2f4a9508c98
David Oliveira <dh.araujo.oliveira@gmail.com>
2025-04-22 16:38:31 -0300

    initial project adjustments

---
+++ src/Ambev.DeveloperEvaluation.Application/Mappings/GetUserResultProfile.cs
@@ -0,0 +1,15 @@
+﻿using Ambev.DeveloperEvaluation.Application.Users.GetUser;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.Application.Mappings
+{
+    public class GetUserResultProfile : Profile
+    {
+        public GetUserResultProfile()
+        {
+            CreateMap<User, GetUserResult>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Username));
+        }
+    }
+}
